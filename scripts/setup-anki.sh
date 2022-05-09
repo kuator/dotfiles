5d13a85fdfc7677c3e223eb936a36fbe75fa8cdc
evakuator <kuatabylkasymov@gmail.com>
2022-05-09 16:18:09 +0600

    update

--- scripts/setup-anki.sh
+++ scripts/setup-anki.sh
@@ -6,6 +6,7 @@ wget https://github.com/ankitects/anki/releases/download/2.1.49/anki-2.1.49-linu
 tar xaf anki-2.1.49-linux.tar.bz2
 cd anki-2.1.49-linux/
 sudo ./install.sh
+mkdir -p ~/.local/share/Anki2/addons21/
 git clone 'https://github.com/Ajatt-Tools/PitchAccent.git' ~/.local/share/Anki2/addons21/pitch_accent
 git clone 'https://github.com/Ajatt-Tools/Furigana.git' ~/.local/share/Anki2/addons21/ajt_furigana
 cp -r $DOTFILES/anki/addons21/*  ~/.local/share/Anki2/addons21
