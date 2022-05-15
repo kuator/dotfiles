9fdc8f6d6f4c49c5e39c87d2c30f025137716b43
evakuator <kuatabylkasymov@gmail.com>
2022-05-15 11:21:30 +0600

    update

--- scripts/xdg-bash.sh
+++ scripts/xdg-bash.sh
@@ -13,7 +13,7 @@ _confdir=${XDG_CONFIG_HOME:-$HOME/.config}/bash
 _datadir=${XDG_DATA_HOME:-$HOME/.local/share}/bash
 
 # Source settings file
-if [ -d "$_confdir" ] then
+if [ -d "$_confdir" ]; then
     for f in bash_profile bashrc; do
         [ -f "$_confdir/$f" ] && . "$_confdir/$f"
     done
