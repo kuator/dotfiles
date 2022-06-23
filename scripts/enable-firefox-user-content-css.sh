5aaed4ea1b4fb7917b3f95cdf2235fd5ea2d3845
evakuator <kuatabylkasymov@gmail.com>
2022-06-23 13:06:35 +0600

    update

---
+++ scripts/enable-firefox-user-content-css.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ~/.mozilla/firefox/
+if [[ $(grep '\[Profile[^0]\]' profiles.ini) ]]
+then PROFPATH=$(grep -E '^\[Profile|^Path|^Default' profiles.ini | grep -1 '^Default=1' | grep '^Path' | cut -c6-)
+else PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
+fi
+
+echo $PROFPATH
+cd $PROFPATH
+
+cat  <<'EOF' > user.js
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+EOF
+
+mkdir -p chrome && cd chrome
+
+cat  <<'EOF' > userContent.css
+@-moz-document regexp("^moz-extension://.*search.html$") {
+  .content { 
+    background-color: #777; 
+  }
+}
+EOF
