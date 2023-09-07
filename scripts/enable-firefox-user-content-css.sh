e2c9c132f1a7b555e79cf27adc24e17153426045
evakuator <kuatabylkasymov@gmail.com>
2023-09-07 16:16:04 +0600

    update

--- scripts/enable-firefox-user-content-css.sh
+++ scripts/enable-firefox-user-content-css.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 cd ~/.mozilla/firefox/
-if [[ $(grep '\[Profile[^0]\]' profiles.ini) ]]
-then PROFPATH=$(grep -E '^\[Profile|^Path|^Default' profiles.ini | grep -1 '^Default=1' | grep '^Path' | cut -c6-)
-else PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
-fi
+
+# if [[ $(grep '\[Profile[^0]\]' profiles.ini) ]]
+# then PROFPATH=$(grep -E '^\[Profile|^Path|^Default' profiles.ini | grep -1 '^Default=1' | grep '^Path' | cut -c6-)
+# else PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
+# fi
+
+PROFPATH=$(grep "Default=.*\.default-release*" "$HOME/.mozilla/firefox/profiles.ini" | cut -d"=" -f2)
 
 echo $PROFPATH
 cd $PROFPATH
