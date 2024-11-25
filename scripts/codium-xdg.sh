4a786d47baff1bde41692a60f53e22284cbf68bd
evakuator <>
2024-11-25 22:50:17 +0500

    update

--- scripts/codium-xdg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DOTFILES="$HOME/dotfiles"
-export XDG_CONFIG_HOME="$HOME/.config"
-
-mkdir -p $XDG_CONFIG_HOME/VSCodium/User
-ln -sv $DOTFILES/codium/keybindings.json $XDG_CONFIG_HOME/VSCodium/User/keybindings.json
-ln -sv $DOTFILES/codium/settings.json $XDG_CONFIG_HOME/VSCodium/User/settings.json
--- scripts/proton_textractor.sh
+++
@@ -1,1 +0,0 @@
-echo 'Remove ~/.config/steamtinkerlaunch often, for some reason it starts working after removing it.'
